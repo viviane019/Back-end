28e47dd242daf0b00b3a2a78ad332b2f6b062c6d
viviane019 <vivianevitoriaa90@gmail.com>
2025-08-01 16:48:58 -0300

    01.08.25

---
+++ Aula01/variaveis.php
@@ -0,0 +1,11 @@
+<?php 
+/*Desolvolva um código com a mesma estrutura que o abaixo, porem com os seus dados.*/
+
+ echo"Olá! \n";
+ $nome = "Viviane! \n";
+ $idade = "16";
+ $ano_atual = "2025";
+ 
+ $data_nasc= $ano_atual-$idade;
+ echo $nome, "você nasceu em:", $data_nasc;
+?> 
